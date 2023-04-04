021b716a7e87434acd0730cee25ad25e07e20219
lunainfotec <kirtimadan1997@gmail.com>
2023-04-04 19:48:41 +0530

    added: random 1 to 6 number

--- src/com/bl/snakeladder/SnakeLadder.java
+++ src/com/bl/snakeladder/SnakeLadder.java
@@ -2,7 +2,15 @@ package com.bl.snakeladder;
 
 public class SnakeLadder {
 
+    int position = 0;
+  static void dieRole(){
+
+      int diaValue = (int)(Math.random()*10 % 6)+1;
+      System.out.println(diaValue);
+
+  }
     public static void main(String[] args) {
-        int position = 0;
+
+             dieRole();
     }
 }
