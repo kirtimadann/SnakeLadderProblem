f1d7200f5270aad68f21e6a4adb7648844f67f9f
lunainfotec <kirtimadan1997@gmail.com>
2023-04-04 20:05:02 +0530

    check Plear position playor play or not

--- src/com/bl/snakeladder/SnakeLadder.java
+++ src/com/bl/snakeladder/SnakeLadder.java
@@ -4,13 +4,30 @@ public class SnakeLadder {
 
     int position = 0;
   static void dieRole(){
-
       int diaValue = (int)(Math.random()*10 % 6)+1;
       System.out.println(diaValue);
+
+  }
+  static  void  play(){
+      int number =(int)(Math.random()*10%3);
+
+      switch (number){
+
+          case 0:
+              System.out.println("No Player Move");
+              break;
+          case 1:
+              System.out.println("Ladder the player moves ahead");
+              break;
+          case 2:
+              System.out.println("Snake the player moves behind");
+              break;
+      }
 
   }
     public static void main(String[] args) {
 
              dieRole();
+             play();
     }
 }
